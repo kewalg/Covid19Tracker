aea92fc0f30be83e91ae9536959b88ee47e7d8dc
kewalg <kewalgulve007@gmail.com>
2020-06-10 15:25:26 -0400

    Implemented RecyclerView/CardView

--- app/src/main/java/com/example/covid19tracker/MainActivity.java
+++ app/src/main/java/com/example/covid19tracker/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.covid19tracker;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.List;
 
 import okhttp3.OkHttpClient;
@@ -16,48 +21,44 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView tv;
+    private DataAdapter dataAdapter;
+    private ArrayList<CountriesItem> countriesItems = new ArrayList<>();
+    private RecyclerView mRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tv = findViewById(R.id.tvtest);
+        mRecyclerView = findViewById(R.id.recycler_view);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        parseJSON();
+    }
+
+    private void parseJSON() {
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("https://api.covid19api.com/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
         RetrofitInterface retrofitInterface = retrofit.create(RetrofitInterface.class);
-
         Call<com.example.covid19tracker.Response> call = retrofitInterface.getStats();
-
         call.enqueue(new Callback<Response>() {
             @Override
             public void onResponse(Call<Response> call, retrofit2.Response<Response> response) {
                 if (!response.isSuccessful()) {
-                    tv.setText("Code: " + response.code());
+                    Toast.makeText(MainActivity.this, "ERROR CODE: " + response.code(), Toast.LENGTH_LONG).show();
                     return;
                 }
-                List<CountriesItem> countriesItemList = response.body().getCountries();
-                for (CountriesItem countriesItem : countriesItemList) {
-                    String content = "";
-                    content += "Country Name: " + countriesItem.getCountry() + "\n";
-                    content += "New Cases: " + countriesItem.getNewConfirmed() + "\n";
-                    content += "Total Cases: " + countriesItem.getTotalConfirmed() + "\n";
-                    content += "New Deaths: " + countriesItem.getNewDeaths() + "\n";
-                    content += "Total Deaths: " + countriesItem.getTotalDeaths() + "\n";
-                    content += "Recovered Today: " + countriesItem.getNewRecovered() + "\n";
-                    content += "Total Recovered: " + countriesItem.getTotalRecovered() + "\n\n";
-                    tv.append(content);
-                }
+
+                countriesItems = new ArrayList<>(response.body().getCountries());
+                dataAdapter = new DataAdapter(countriesItems, MainActivity.this);
+                mRecyclerView.setAdapter(dataAdapter);
             }
 
             @Override
             public void onFailure(Call<Response> call, Throwable t) {
-                tv.setText(t.getMessage());
-
+                Toast.makeText(MainActivity.this, "Error" + t.getMessage(), Toast.LENGTH_LONG).show();
             }
         });
     }
